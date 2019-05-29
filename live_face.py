b405c1d42ae29975eb2b76c72a54ec425314f105
RocketRikky <39258575+RocketRikky@users.noreply.github.com>
2019-05-29 22:10:48 +0530

    Create live_face.py

---
+++ live_face.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+
+face_classifier = cv2.CascadeClassifier('cascade_files_for_object_detection\\haarcascade_frontalface_default.xml')
+eye_classifier = cv2.CascadeClassifier('cascade_files_for_object_detection\\haarcascade_eye.xml')
+
+def face_detector(image, size=0.5):
+
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+	faces = face_classifier.detectMultiScale(gray, 1.2, 5)
+
+	if faces is ():
+		return image
+
+	for (x,y,w,h) in faces:
+		x = x-50
+		w= w+50
+		y= y-50
+		h= h+50
+
+		cv2.rectangle(image, (x,y),(x+w,y+h),(255,0,0),2)
+		roi_gray = gray[y:y+h, x:x+w]
+		roi_color = image[y:y+h, x:x+w]
+
+		eyes = eye_classifier.detectMultiScale(roi_gray)
+		for (ex,ey,ew,eh) in eyes:
+			cv2.rectangle(roi_color, (ex,ey),(ex+ew,ey+eh),(0,255,255),2)
+
+		roi_color = cv2.flip(roi_color,1)
+		return roi_color
+
+cap = cv2.VideoCapture(0)
+
+while True:
+
+	ret, frame = cap.read()
+	cv2.imshow('Face Detector ', face_detector(frame))
+
+	if cv2.waitKey(1) ==13:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
